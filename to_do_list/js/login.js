f724156936f1e9149c315574bbb70c4d3292071f
jd1747 <44023616+jd1747@users.noreply.github.com>
2022-01-14 22:27:30 +0900

    (incomplete) added a portfolio

--- to_do_list/js/login.js
+++ to_do_list/js/login.js
@@ -1,16 +1,27 @@
 const loginForm = document.querySelector(".login");
 const loginInput = document.querySelector(".login__input");
-const mainContent = document.querySelector(".main-content");
 const greeting = document.querySelector(".main-content__greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
-function loginSubmit(event) {
+function onLoginSubmit(event) {
   event.preventDefault();
-  localStorage.setItem("userName", loginInput.value);
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
   loginForm.classList.add(HIDDEN_CLASSNAME);
-  mainContent.classList.remove(HIDDEN_CLASSNAME);
-  greeting.innerHTML = `Hello, ${localStorage.getItem("userName")}!`;
+  userGreetings(username);
 }
 
-loginForm.addEventListener("submit", loginSubmit);
+function userGreetings(username) {
+  greeting.innerText = `Hello, ${username}!`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername) {
+  userGreetings(savedUsername);
+} else {
+  loginForm.addEventListener("submit", onLoginSubmit);
+}
